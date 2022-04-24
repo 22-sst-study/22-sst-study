31068d9c3e4016c5f9c37c77061563c1ec87e2b9
SooJeong Lee <leesujung0917@gmail.com>
2022-04-24 15:01:11 +0900

    Update 16234_인구이동_1.py

--- week06/n16234/doodung/16234_인구이동_1.py
+++ week06/n16234/doodung/16234_인구이동_1.py
@@ -1,8 +1,10 @@
 from collections import deque
 
 # 16234 인구이동
-# 2022-04-17 07:28~08:30
 # python3 시간초과 pypy3 1340ms
+# bfs
+# 어려웠던 점 : 인구 이동이 없을 경우 종료 조건
+# 시간 줄일 방법은 ????
 
 N, L, R = map(int, input().split())
 board = [list(map(int, input().split())) for i in range(N)]
@@ -15,14 +17,15 @@ def bfs(x, y):
     q = deque()
     q.append((x, y))
     visited[x][y] = True
-    union = [(x, y)]
-    peo = board[x][y]
-    cnt = 1
+    union = [(x, y)]  # 연합
+    peo = board[x][y]  # 인구수
+    cnt = 1  # 연합된 국가 수
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            # 인구 차이가 L명 이상 R명 이하라면, 연합 국가에 넣기
             if 0 <= nx < N and 0 <= ny < N and L <= abs(board[nx][ny] - board[x][y]) <= R and not visited[nx][ny]:
                 visited[nx][ny] = True
                 q.append((nx, ny))
@@ -30,12 +33,14 @@ def bfs(x, y):
                 peo += board[nx][ny]
                 cnt += 1
 
+    # 연합을 이루고 있는 각 칸의 인구수
     for i, j in union:
         board[i][j] = peo // cnt
 
     return len(union)
 
 
+# 인구 이동 없을 때까지 반복
 while True:
     flag = False
     visited = [[False] * N for _ in range(N)]
@@ -45,6 +50,7 @@ while True:
                 if bfs(i, j) > 1:
                     flag = True
 
+    # 지금까지 인구 이동 없을 경우 그만하기
     if not flag:
         break
 
